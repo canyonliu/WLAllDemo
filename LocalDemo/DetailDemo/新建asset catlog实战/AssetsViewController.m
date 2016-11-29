e64c477bb00112bb7547724364b8042f6b145005
liuqingcan <liuqingcan@baidu.com>
2016-11-29 15:13:22 +0800

    解释为什么图片名称只能使用XXX或者XXX.png

--- LocalDemo/DetailDemo/新建asset catlog实战/AssetsViewController.m
+++ LocalDemo/DetailDemo/新建asset catlog实战/AssetsViewController.m
@@ -26,7 +26,12 @@
 - (UIImageView *)assetsImage{
     if (!_assetsImage) {
         _assetsImage = [[UIImageView alloc]initWithFrame:CGRectMake(50, 100, self.view.bounds.size.width - 100, 200)];
-        [_assetsImage setImage:[UIImage imageNamed:@"guide_picture_page3"]];
+        //使用guide_word_page3.png 和 guide_word_page3都是可以的，但是就是不能使用
+        //        guide_word_page3@2x.png,尽管在xcassets中是含有2x的，但是在json的文件描述之中：
+//        | "filename" : "guide_word_page3@2x.png",
+//        |  "scale" : "2x"
+//        是会把名为guide_word_page3@2x.png的图片自动的放到手机为2x的中去
+        [_assetsImage setImage:[UIImage imageNamed:@"guide_word_page3.png"]];
     }
     return _assetsImage;
 }
